05404ad370ad9edd66a528999e1047ec113b3be6
philliy <34437237+philliy@users.noreply.github.com>
2023-04-13 00:06:39 +0300

    script main update amharic time our

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ var x = setInterval(function() {
     // If the count down is over, write some text 
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("time").innerHTML = "Bless the married couple for happy life!";
+        document.getElementById("time").innerHTML = "ይሆዋን ለ ታላቅ ስጦታው እናመሰግናለን 👏👏👏!";
     }
 }, 1000);
 
